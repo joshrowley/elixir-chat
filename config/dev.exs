a5620ad52c281c25afe912495e6fd1483387a78e
Josh Rowley <joshua.f.rowley@gmail.com>
2016-01-29 00:06:14 -0500

    config db

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :elixir_chat, ElixirChat.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "elixir_chat_dev",
   hostname: "localhost",
   pool_size: 10
